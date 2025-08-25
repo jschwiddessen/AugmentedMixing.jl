1323cbf9e896f445cc59d61cf894d6118cc084b8
Documenter.jl <documenter@juliadocs.github.io>
2025-08-25 09:59:25 +0000

    build based on 442be5f

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#AugmentedMixing","page":"Home","title":"AugmentedMixing","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for AugmentedMixing.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
